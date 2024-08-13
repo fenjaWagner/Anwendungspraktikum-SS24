a4259f7ce84bdebf755efebf0d9336c49eb8f79b
Fenja Wagner <fenja.wagner@uni-jena.de>
2024-08-13 13:30:55 +0200

    storing plots for each order

--- src/evaluation/data_evaluation.py
+++ src/evaluation/data_evaluation.py
@@ -71,10 +71,10 @@ class DataEvaluation:
 
         ax.bar(self.detail_modes, percentage, label=self.detail_modes)
         ax.set_ylabel('Percentage')
-        ax.set_title('Percentage of correct answers per game mode of user '+ user)
+        ax.set_title(f'Percentage of correct answers in order {self.order} of {user}.')
         ax.set_xticklabels(self.detail_modes, rotation=45, ha='right')  # Rotate the labels by 45 degrees
         ax.set_ylim(0, max(max_value, 25) + 5)
-        plt.savefig("../result_data/plots/"+user+".png", format="png", dpi=300, bbox_inches="tight")
+        plt.savefig("../result_data/plots/"+user+"-"+self.order +".png", format="png", dpi=300, bbox_inches="tight")
 
     def print_csv(self):
         """Prints the data dictionary into a csv file.
